e8369d2ecd7c20822491ccc98e9a6de2fb08f183
Ihar Vypau <igor.vypov@gmail.com>
2018-09-14 23:41:38 +0300

    advanced-zeros task implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+ const getPrimeFactors = _number => {
+   let primes = [];
+   for(i =2; i <= _number;i++){
+     while((_number %i ) === 0){
+       primes.push(i);
+       _number /= i;
+     }
+   }
+   return primes;
+ };
+
+ const getNumberFactors = (_number, prime) =>{
+  let res = 0;
+  while(_number > 0){
+    _number = Math.floor(_number / prime );
+    res += _number;
+  }
+  return res;
+ };
+  
+ let primeFactors = getPrimeFactors(base); 
+ if(primeFactors.length > 0){
+   let degree = 1, result=0;
+   primeFactors.reduce( (prev,val) => {
+    let factors_count = getNumberFactors(number,val);
+     if(prev === val){
+       degree++;
+       factors_count = Math.floor(factors_count/degree);
+     }else{
+       degree = 1;
+     }
+      result = (result > factors_count || result === 0) ? factors_count : result;
+      return val;
+   },0);
+   return result;
+ }
+ return 0;
+};
+
